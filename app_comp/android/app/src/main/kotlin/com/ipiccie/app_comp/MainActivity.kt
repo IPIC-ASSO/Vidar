136021553efde6204fc54daf8dc6bddea0c57c4e
I-I <92298342+idrisseI@users.noreply.github.com>
2024-01-06 21:48:26 +0100

    v2.1.7 patch 1

---
+++ app_comp/android/app/src/main/kotlin/com/ipiccie/app_comp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ipiccie.app_comp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- app_comp/android/app/src/main/kotlin/com/ipiccie/muetssages/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ipiccie.muetssages
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
